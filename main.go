99324740d1eb47d0975376f08921403e8d0379cd
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-08-17 23:57:55 +0700

    param and queryparam route

--- main.go
+++ main.go
@@ -10,12 +10,30 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
+	router.GET("/book/:id", bookHandler)
+	router.GET("/books", queryHandler) 
 
 	router.Run()
 }
 
-func rootHandler(c *gin.Context){
+func rootHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Hello World",
+	})
+}
+
+func bookHandler(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{
+		"Book ID ": id,
+	})
+}
+
+func queryHandler(c *gin.Context){
+	title := c.Query("title")
+	id := c.Query("id")
+	c.JSON(http.StatusOK, gin.H{
+		"book Id ": id,
+		"Book Title ": title,
 	})
 }
